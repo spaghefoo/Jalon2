049445fc7d5ef46ea4b7ddcaaefc44eeea18d12e
Théophane <theophane.legrand59@gmail.com>
2022-11-21 15:51:16 +0100

    modified:   modele/authen.inc.php 	modified:   modele/utilisateur.inc.php

--- modele/authen.inc.php
+++ modele/authen.inc.php
@@ -1,3 +1,55 @@
 <?php
 //Ici seront créer les fonctions qui permettent la connexion et la deconnexion
+include_once "utilisateur.int.php";
+
+function login($mailU, $mdpU) {
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+
+    $util = getUtilisateurByMailU($mailU);
+    $mdpBD = $util["mdpU"];
+
+    if (trim($mdpBD) == trim(crypt($mdpU, $mdpBD))) {
+        // le mot de passe est celui de l'utilisateur dans la base de donnees
+        $_SESSION["mailU"] = $mailU;
+        $_SESSION["mdpU"] = $mdpBD;
+    }
+}
+
+function logout() {
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+    unset($_SESSION["mailU"]);
+    unset($_SESSION["mdpU"]);
+}
+
+function getMailULoggedOn(){
+    if (isLoggedOn()){
+        $ret = $_SESSION["mailU"];
+    }
+    else {
+        $ret = "";
+    }
+    return $ret;
+        
+}
+
+function isLoggedOn() {
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+    $ret = false;
+
+    if (isset($_SESSION["mailU"])) {
+        $util = getUtilisateurByMailU($_SESSION["mailU"]);
+        if ($util["mailU"] == $_SESSION["mailU"] && $util["mdpU"] == $_SESSION["mdpU"]
+        ) {
+            $ret = true;
+        }
+    }
+    return $ret;
+}
+
 ?>
--- modele/utilisateur.inc.php
+++ modele/utilisateur.inc.php
@@ -1,3 +1,40 @@
 <?php
-include_once "";
+include_once "bd.inc.php";
+
+function getUtilisateurs() {
+    $resultat = array();
+
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from utilisateur");
+        $req->execute();
+
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        while ($ligne) {
+            $resultat[] = $ligne;
+            $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        }
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function getUtilisateurByMailU($mailU) {
+    $resultat = array();
+
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from utilisateur where mailU=:mailU");
+        $req->bindValue(':mailU', $mailU, PDO::PARAM_STR);
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
 ?>
