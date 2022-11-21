a3e9fadaf72b7b0c891981f45c19a591db88cbbc
Théophane <theophane.legrand59@gmail.com>
2022-11-21 16:46:16 +0100

    Modification 16:46

---
+++ controleur/connexion.php
@@ -0,0 +1,29 @@
+<?php
+if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
+    $racine="..";
+}
+include_once "$racine/modele/authen.inc.php";
+
+if (isset($_POST["mailU"]) && isset($_POST["mdpU"])){
+    $mailU=$_POST["mailU"];
+    $mdpU=$_POST["mdpU"];
+} 
+else {
+    $mailU="";
+    $mdpU="";
+}
+
+login($mailU,$mdpU);
+
+if (isLoggedOn()){ // si l'utilisateur est connecté on redirige vers le controleur monProfil
+    include "$racine/controleur/monProfil.php";
+}
+else{ // l'utilisateur n'est pas connecté, on affiche le formulaire de connexion
+    // appel du script de vue 
+    $titre = "authentification";
+    include "$racine/vue/entete.html.php";
+    include "$racine/vue/vueAuthentification.php";
+    include "$racine/vue/pied.html.php";
+}
+
+?>
