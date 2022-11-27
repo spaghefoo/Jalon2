31c9790c5dbf25913a6ac235f670df1e6003607d
spaghefoo <sofiane59660@gmail.com>
2022-11-27 18:31:26 +0100

    test/

--- modele/destination.inc.php
+++ modele/destination.inc.php
@@ -8,27 +8,51 @@ function getTraversees()
  // A COMPLETER
  try
  {
- $co = connexionPDO();  
+    $traversees = array();
+    $co = connexionPDO();  
+    $request = $co->query("SELECT * FROM traversee");
+
+
+    while($tableau = $request->fetch(PDO::FETCH_ASSOC))
+    {
+        $traversees[] = $tableau;
+    }
  }
  catch(PDOException $e)
  {
     $message = $e->getMessage();
+    $traversees = false;
  }
-
+ finally{
+    $co = null;
+ }
+ return $traversees;
 }
 
 // sofiane - permet d'avoir une traversée par un numéro
-function getTraverseesBynumero()
+function getTraverseesBynumero($numero)
 {
     // A COMPLETER AS WELL
     try
     {
     $co = connexionPDO();
+    $co->prepare("SELECT * FROM traversee WHERE numeroTraversee = ?");
+    $co->execute([$numero]);
+
+    while($tableau = $request->fetch(PDO::FETCH_ASSOC))
+    {
+        $traversee[] = $tableau;
+    }
     }
     catch(PDOException $e)
     {
         $message = $e->getMessage();
     }
+    finally
+    {
+        $co = null;
+    }
+    return $traversee;
 }
 
 ?>
