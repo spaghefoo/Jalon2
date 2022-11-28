3fad48bdc25af895ce1b6e6583c944496feeb699
Sofiane SA. Acheraiou <sofiane.acheraiou@sio.lan>
2022-11-28 11:37:02 +0100

    sofiane - changement de style page inscription 	modified:   vue/vueInscription.php

--- vue/vueInscription.php
+++ vue/vueInscription.php
@@ -1,13 +1,18 @@
-<h1>Inscription</h1>
-
-<form action="?action=inscription" method="POST">
+<section class="section">
+    <div id="connexionTitre">
+        <h1>Inscription</h1>
+    </div>
+<div class="container">
+    <div class="card">
+        <div class="content">
+<form id="centre" action="?action=inscription" method="POST">
 <!-- sofiane - j'ai fait le formulaire d'inscription avec les valeurs qui correspondent.-->
-<input type="text" name="email" placeholder="Email de connexion" /> <br />
-<input type="number" name="codePostal" placeholder="Code Postal" /><br />
-<input type="text" name="nomVille" placeholder="Ville" /></br>
-<input type="text" name="nom" placeholder="Votre nom" /><br />
-<input type="password" name="password" placeholder="Mot de passe" /> <br />
-<input type="submit" />
+    <input type="text" name="email" placeholder="Email de connexion" /> <br />
+    <input type="number" name="codePostal" placeholder="Code Postal" /><br />
+    <input type="text" name="nomVille" placeholder="Ville" /></br>
+    <input type="text" name="nom" placeholder="Votre nom" /><br />
+    <input type="password" name="password" placeholder="Mot de passe" /> <br />
+    <input type="submit" />
 
 </form>
 <p><?php 
@@ -17,5 +22,9 @@ if(isset($message))
 echo $message; 
 }
 ?></p>
+        </div>
+    </div>
+</div>
 <br />
 <a href="?action=connexion">Déja un compte? Connexion.</a>
+</section>
