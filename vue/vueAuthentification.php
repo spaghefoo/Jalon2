cec6b68089ab43aa8459dbabade0bc760a32190f
Théophane <theophane.legrand59@gmail.com>
2023-03-06 14:05:15 +0100

    modified:   vue/vueAuthentification.php

--- vue/vueAuthentification.php
+++ vue/vueAuthentification.php
@@ -2,23 +2,21 @@
 
 <section class="section">
     <div id="connexionTitre">
-        <h1>Connexion</h1>
+        <h1>Connexion</h1> <br><br><br><br><br>
     </div>
     <div class="container">
-        <div class="card">
-            <div class="content">
-                <form id="centre" action="./?action=connexion" method="POST">
-                    <p>Email</p>
-                    <input type="text" id="connexion_text" name="mailU" placeholder="" /> <br /><br />
+        <div class="content">
+            <form id="centre" action="./?action=connexion" method="POST">
+                <br><br> Email <br>
+                <input type="text" id="connexion_text" name="mailU" placeholder="" /> <br /><br />
 
-                    <p>Password</p>
-                    <input type="password" id="connexion_password" name="mdpU" placeholder="" /> <br /><br />
-                    <input type="submit" id="connexion_submit" />
-                </form>
-                <br><br>
-                <p>Pas de compte ? <a href="?action=inscription"><b>Inscription</b></a></p>
-                <br />
-            </div>
+                Password <br>
+                <input type="password" id="connexion_password" name="mdpU" placeholder="" /> <br /><br />
+                <input type="submit" id="connexion_submit" />
+            </form>
+            <br><br>
+            <p>Pas de compte ? <a href="?action=inscription"><b>Inscription</b></a></p>
+            <br />
         </div>
     </div>
 
