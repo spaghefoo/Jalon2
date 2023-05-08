c0f14a2612521c9a5e8af7d9ae37e9a79b59c496
sofiane@acheraiou.fr <sofiane>
2023-05-08 15:56:35 +0200

    Correction petite erreur dans l'action de logout.

--- controleur/deconnexion.php
+++ controleur/deconnexion.php
@@ -3,6 +3,7 @@ if($_SERVER["SCRIPT_FILENAME"] ==  __FILE__){
     $racine="..";
 } // 1 - permet de naviguer entre les dossiers
 include_once "$racine/modele/authen.inc.php";
+$titre = "Vous êtes déconnecté.";
 
 // 2 - recuperation des donnees GET, POST, et SESSION
 ;
